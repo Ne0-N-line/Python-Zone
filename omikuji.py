960bf317b37a2936e92166e12f8c3a957d4c8c90
Ne0-N-line <syado6664@gmail.com>
2022-04-03 20:03:40 +0900

    おみくじの内容を細分化

--- omikuji.py
+++ omikuji.py
@@ -1,14 +1,18 @@
 import random
-omikuji = random.randint(1,10) # 1から10の数値をランダムに生成
+omikuji = random.randint(1,20) # 1から20の数値をランダムに生成
 #print(omikuji)
 
 if omikuji == 1:
     print("大吉")
-elif omikuji == 2:
+elif omikuji <= 4: # 2,3,4
     print("中吉")
-elif omikuji <= 4: # 3,4
+elif omikuji <= 8: # 5,6,7,8
     print("小吉")
-elif omikuji <= 7: # 5,6,7
+elif omikuji <= 12: # 9,10,11,12
+    print("吉")
+elif omikuji <= 15: # 13,14,15
+    print("末吉")
+elif omikuji <= 18: # 16,17,18
     print("凶")
-else:              # 8,9,10
+else:              # 19,20
     print("大凶")
